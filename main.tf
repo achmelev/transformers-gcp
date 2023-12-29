76b7e697a825d125dce16b85dc4adad2ccdd4750
Alexei Chmelev <alexei_chmelev@gmx.de>
2023-12-29 18:12:54 +0100

    Grundgeruest

---
+++ main.tf
@@ -0,0 +1,92 @@
+####################Variablen######################
+
+variable "region" {
+  type = string
+  default = "europe-west1"
+}
+
+variable "zone" {
+  type = string
+  default = "europe-west1-c"
+}
+
+variable "machine_type" {
+  type = string
+  #Ubuntu 22.04
+  default = "e2-micro"
+}
+
+variable "image" {
+  type = string
+  #Ubuntu 22.04
+  default = "ubuntu-os-cloud/ubuntu-2204-lts"
+}
+
+variable "disk_size" {
+  type = number
+  default = 40
+}
+
+variable "instance_user" {
+  type = string
+  default = "alexei_chmelev"
+}
+
+variable "private_key" {
+  type = string
+  default = "Transformers.pem"
+}
+
+variable "install_script" {
+  type = string
+  default = "nothing.sh"
+}
+
+
+provider "google" {
+  project     = "transformers-409614"
+  region      = var.region
+  zone        = var.zone
+}
+resource "google_compute_instance" "transformers" {
+  provider = google
+  name = "transformers"
+  machine_type = var.machine_type
+
+  metadata = {
+    ssh-keys = "${var.instance_user}:${file("Transformers.pub")}"
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+      size = var.disk_size
+    }
+  }
+  
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
+
+  provisioner "local-exec" {
+    command = "scp -o StrictHostKeyChecking=no -i ${var.private_key} ${var.install_script} ${var.instance_user}@${self.network_interface.0.access_config.0.nat_ip}:${var.install_script}"
+  }
+  provisioner "local-exec" {
+    command = "ssh -o StrictHostKeyChecking=no -i ${var.private_key} ${var.instance_user}@${self.network_interface.0.access_config.0.nat_ip} \"chmod 755 ${var.install_script}\""
+  }
+
+  provisioner "local-exec" {
+    command = "ssh -o StrictHostKeyChecking=no -i ${var.private_key} ${var.instance_user}@${self.network_interface.0.access_config.0.nat_ip} ./${var.install_script}"
+  }
+  
+}
+
+output "PublicIP" {
+    value = google_compute_instance.transformers.network_interface.0.access_config.0.nat_ip
+    description = "The public IP address of the newly created instance"
+}
